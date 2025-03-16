dda0ffc0faa636f4cdb442bd2f052aa84419494c
kasun <kasungunathilaka085@gmail.com>
2025-03-16 19:43:08 +1100

    name tester

---
+++ name_tester.py
@@ -0,0 +1,12 @@
+def main():
+
+    name = input("Please enter your name: ")
+
+
+    if name == "YourName":
+        print("Awesome name!")
+    else:
+        print("Silly name!")
+
+if __name__ == "__main__":
+    main()
