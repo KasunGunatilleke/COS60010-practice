ed123d9f9b6bd61b7a52fd299420384df603b1cc
kasun <kasungunathilaka085@gmail.com>
2025-03-16 19:48:07 +1100

    name tester is changed

--- name_tester.py
+++ name_tester.py
@@ -1,12 +1,19 @@
 def main():
+    print("=====")
+
+    i = 0
 
     name = input("Please enter your name: ")
 
+    print(f"{name} is a ", end="")
 
-    if name == "YourName":
-        print("Awesome name!")
-    else:
-        print("Silly name!")
+    while i < 10:
+
+        print("silly ", end="")
+
+        i += 1
+
+    print("name!")
 
 if __name__ == "__main__":
     main()
